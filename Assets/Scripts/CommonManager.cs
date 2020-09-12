cd46f2644718e2e936e23252178a587b51002979
mao-yao <murasakinoaoguma@gmail.com>
2020-09-12 11:25:16 +0900

    共通部分追加

---
+++ Assets/Scripts/CommonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonManager : MonoBehaviour
+{
+    //ゲームに共通するスクリプト
+    //・タイトルへ戻るボタン
+    //・メッセージ（メッセージウィンドウ）
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Stage1Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1Manager : MonoBehaviour
+{
+    //居住区
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
