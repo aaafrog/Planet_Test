a42bf34165034f36cf93c0fd20c20b84a1d4dd58
wataru-sekiguchi <late.smith.48@gmail.com>
2020-10-18 19:50:07 +0900

    関口

--- Assets/Scripts/CommonManager.cs
+++ Assets/Scripts/CommonManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 public class CommonManager : MonoBehaviour
 {
@@ -8,16 +10,31 @@ public class CommonManager : MonoBehaviour
     //・タイトルへ戻るボタン
     //・メッセージ（メッセージウィンドウ）
 
+    //外部CSVをListに格納する
+    TextAsset csvFile;  // CSVファイル
+    public List<string[]> Scenarios = new List<string[]>();
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        
-    }
+        // CSVを読み込み、一行ずつListに追加する
+        csvFile = Resources.Load("Message") as TextAsset; // Resouces下のCSV読み込み
+        StringReader reader = new StringReader(csvFile.text);
 
-    // Update is called once per frame
-    void Update()
-    {
+        // リストに追加していく
+        while (reader.Peek() != -1) // reader.Peaekが-1になるまで
+        {
+            string line = reader.ReadLine(); // 一行ずつ読み込み
+            Scenarios.Add(line.Split(',')); // , 区切りでリストに追加
+        }
+
+        // Scenarios[行][列]を指定して値を自由に取り出せる
+        Debug.Log(Scenarios[0][0]);
+        Debug.Log("<color=red>(・∀・)ｲｲ!!</color>");
         
+
     }
+    public string testtext = "あああ";
+
+
 }
--- Assets/Scripts/OpeningManager.cs
+++ Assets/Scripts/OpeningManager.cs
@@ -1,18 +1,136 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpeningManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //*******************************************************************
+    //                Unityで設定
+    //*******************************************************************
+
+    /// メッセージパネル（ボタン）
+    public GameObject MessagePanel;
+
+    /// テキスト
+    public Text text;
+
+    //*******************************************************************
+    //                情報と基本メソッド
+    //*******************************************************************
+
+    /// 書くスピード(短いほど早い)
+    public float writeSpeed = 0.2f;
+
+    /// 書いている途中かどうか
+    public bool isWriting = false;
+
+    /// 文章の番号は Key で表す
+    public int key = 0;
+
+    /// テキストを書くメソッド
+    public void Write(string s)
     {
-        
+        //毎回、書くスピードを 0.2 に戻す------<戻したくない場合はここを消す>
+        writeSpeed = 0.2f;
+
+        StartCoroutine(IEWrite(s));
     }
 
-    // Update is called once per frame
-    void Update()
+    /// テキストを消すメソッド
+    public void Clean()
     {
-        
+        text.text = "";
     }
+
+    //*******************************************************************
+    //                表示するメッセージ
+    //*******************************************************************
+    public CommonManager commonManager;
+    static Dictionary<int, string> message = new Dictionary<int, string>()
+    { 
+        //----\nは改行を表す----
+        { 1, "おはようございます、これは1番目のメッセージです" },
+        { 2, "\nこんにちは、これは2番目のメッセージです" },
+        { 3, "こんばんは、これは3番目のメッセージです" },
+        { 4, "\nおやすみなさい、これは4番目のメッセージです" },
+        { 999, "\nこれでメッセージの表示を終わります" },
+    };
+
+    //*******************************************************************
+    //                メッセージパネルがタッチされた時の処理
+    //*******************************************************************
+    int i = -1;
+    public void Start()
+    {
+        i++;
+        Clean();
+        Write(commonManager.Scenarios[i][1]);
+        Debug.Log("デフォルト");
+    }
+
+    //このメソッドが呼ばれる
+    public void OnClick()
+    {
+       //i++;
+        //Debug.Log(i);
+        //string aaa = commonManager.testtext;
+        //Debug.Log(commonManager.Scenarios[0][1]);
+        //前のメッセージを書いている途中かどうかで分ける
+        Debug.Log("CCC");
+        if (i <= 2)
+        {
+            if (isWriting)
+            {
+                //書いている途中にタッチされた時------------------------------
+
+                //スピード(かかる時間)を 0 にして、すぐに最後まで書く
+                writeSpeed = 0f;
+                Debug.Log("全表示");
+
+            }
+            else
+            {
+                Clean();
+                Write(commonManager.Scenarios[i][1]);
+                Debug.Log("書く");              
+            }
+        }
+        else {
+            //メッセージパネルを消す
+            Debug.Log("メッセージパネルを消す");
+            MessagePanel.SetActive(false);
+        }
+        //i++;
+    }
+
+    //*******************************************************************
+    //                その他
+    //*******************************************************************
+
+    /// 書くためのコルーチン
+    IEnumerator IEWrite(string s)
+    {
+        //書いている途中の状態にする
+        isWriting = true;
+        //渡されたstringの文字の数だけループ
+        for (int i = 0; i < s.Length; i++)
+        {
+            //テキストにi番目の文字を付け足して表示する
+            text.text += s.Substring(i, 1);
+            //次の文字を表示するまで少し待つ
+            yield return new WaitForSeconds(writeSpeed);
+        }
+        //書いている途中の状態を解除する
+        isWriting = false;
+    }
+
+    /// ゲームスタート時の処理
+    // void Start()
+    //{
+    //メッセージパネルに書かれている文字を消す
+    //Clean();
+    //}
 }
