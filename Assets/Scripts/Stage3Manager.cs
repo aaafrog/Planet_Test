409b89341e8347250f48c7103ef005fea4b73e26
mao-yao <murasakinoaoguma@gmail.com>
2020-11-14 19:12:31 +0900

    Stage3修正したヨ

--- Assets/Scripts/Stage3Manager.cs
+++ Assets/Scripts/Stage3Manager.cs
@@ -9,6 +9,7 @@ public class Stage3Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //別のシーンを読み込むよ
         SceneManager.LoadScene("ControllerItem", LoadSceneMode.Additive);
         SceneManager.LoadScene("ControllerMenu", LoadSceneMode.Additive);
     }
